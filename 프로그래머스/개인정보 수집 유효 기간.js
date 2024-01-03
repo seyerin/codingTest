1354306e020f63e4b7ba52c1513898f11a3d1ca3
seoyerin <seyerin12@naver.com>
2024-01-03 23:52:24 +0900

    코테 2 품

--- 프로그래머스/개인정보 수집 유효 기간.js
+++ 프로그래머스/개인정보 수집 유효 기간.js
@@ -1,78 +1,74 @@
 function solution(today, terms, privacies) {
   var answer = [];
 
-  let temp = [];
-  let privaciesYear = [];
-  let privaciesMonth = [];
-  let privaciesDay = [];
-  let privaciesTerms = [];
-
-  let termsArr = [];
-
-  privacies.forEach((x) => {
-    let date = x.replaceAll(".", "");
-    privaciesYear.push(Number(date.slice(0, 4)));
-    privaciesMonth.push(Number(date.slice(4, 6)));
-    privaciesDay.push(Number(date.slice(6, 8)));
+  privacies.forEach((x, n) => {
+    let privaciesSplit = x.split(" "); // 공백 문자를 기준으로 개인정보 수집 날짜와 약관을 나눔
+    // let privaciesDate = privaciesSplit[0].split("."); // .을 기준으로 개인정보 수집 날짜를 년, 월, 일로 나누어줌
 
     terms.forEach((y) => {
-      if (x.slice(x.length - 1) == y.slice(0, 1)) {
-        privaciesTerms.push(Number(y.slice(y.length - 2, y.length)));
+      let termsSplit = y.split(" "); // 공백 기준으로 약관과 보관 날짜 나눔
+      if (termsSplit[0] == privaciesSplit[1]) {
+        let modifiedNum = dateFunc(privaciesSplit[0], termsSplit[1]);
+
+        const todayDate = new Date(today);
+        const modifiedDate = new Date(modifiedNum);
+
+        todayDate.setDate(todayDate.getDate() + 1);
+        modifiedDate.setDate(modifiedDate.getDate() + 1);
+
+        if (todayDate > modifiedDate) {
+          answer.push(n + 1);
+        }
       }
     });
   });
-  terms.forEach((x) => {
-    termsArr[Number(x.slice(x.length - 2))] = Number(x.slice(x.length - 2));
-  });
-
-  for (let i = 0; i < privacies.length; i++) {
-    if (privaciesMonth[i] + termsArr[privaciesTerms[i]] > 12) {
-      privaciesYear[i]++;
-      privaciesMonth[i] = privaciesMonth[i] + termsArr[privaciesTerms[i]] - 12;
-      temp.push(
-        String(privaciesYear[i]) + String(privaciesMonth[i]) + privaciesDay[i]
-      );
-    } else {
-      privaciesMonth[i] = privaciesMonth[i] + termsArr[privaciesTerms[i]];
-      temp.push(
-        String(privaciesYear[i]) + String(privaciesMonth[i]) + privaciesDay[i]
-      );
-    }
-  }
-
-  for (let i = 0; i < temp.length; i++) {
-    if (temp[i].slice(temp[i].length - 1) - 1 == 0) {
-      privaciesMonth[i]--;
-      privaciesDay[i] = 28;
-      temp[i] =
-        String(privaciesYear[i]) + String(privaciesMonth[i]) + privaciesDay[i];
-    } else {
-      temp[i]--;
-    }
-  }
-
-  today = today.replaceAll(".", "");
-  let now = today.slice(0, 4);
-  now = now + Number(today.slice(4, 6));
-  now = now + Number(today.slice(6, 8));
-
-  for (let i = 0; i < privacies.length; i++) {
-    if (temp[i] == now) {
-      continue;
-    } else if (String(temp[i]).slice(0, 4) < now.slice(0, 4)) {
-      answer.push(i + 1);
-    } else if (
-      String(temp[i]).slice(0, 4) == now.slice(0, 4) &&
-      String(temp[i]).slice(4, 5) <= now.slice(4, 5) &&
-      String(temp[i]).slice(5, 7) < now.slice(5, 7)
-    ) {
-      answer.push(i + 1);
-    }
-  }
-  console.log(temp);
   console.log(answer);
-  answer = answer.sort();
   return answer;
+}
+
+function dateFunc(privacie, term) {
+  let splitNum;
+  let year;
+  let month;
+  let day;
+  let termDay = 28 * term; // 한달을 28일만 있다 가정
+  let result;
+
+  splitNum = privacie.split(".");
+
+  year = splitNum[0];
+  month = splitNum[1];
+  day = splitNum[2];
+
+  let cnt = 1;
+  while (termDay > cnt) {
+    // 주어진 term만큼 반복하며 day 더해줌
+    day++;
+    if (day == 29) {
+      day = 1;
+      month++;
+      if (month > 12) {
+        month = 1;
+        year++;
+      }
+    }
+    cnt++;
+  }
+
+  year = String(year);
+  month = String(month);
+  day = String(day);
+
+  //month, day의 길이가 1일  때 앞에 0이 붙음
+  if (month.length == 1) {
+    month = 0 + month;
+  }
+  if (day.length == 1) {
+    day = 0 + day;
+  }
+
+  result = year + "." + month + "." + day;
+  return result;
 }
 
 const today = "2022.05.19";
