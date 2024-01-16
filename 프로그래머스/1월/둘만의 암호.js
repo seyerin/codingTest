b4b17ad7949f9ef862c4b590042adf11953f989c
seoyerin <seyerin12@naver.com>
2024-01-16 22:26:05 +0900

    코테15

--- 프로그래머스/1월/둘만의 암호.js
+++ 프로그래머스/1월/둘만의 암호.js
@@ -32,29 +32,25 @@ function solution(s, skip, index) {
   for (let i = 0; i < s.length; i++) {
     split.push(s[i]);
   }
-
   split.forEach((x) => {
-    // console.log(x);
     let num = alpha.findIndex((y) => x == y);
     let cnt = 0;
     let current;
     while (cnt != index) {
-      current = index <= 1 ? alpha[num + 1] : alpha[num];
+      if (num == alpha.length - 1) {
+        num = -1;
+        if (index <= 1) {
+          current = alpha[num];
+        }
+      }
+      current = alpha[num + 1];
       if (skip.indexOf(current) == -1) {
         cnt++;
         num++;
       } else {
         num++;
       }
-      if (num >= alpha.length) {
-        num = 0;
-        if (index <= 1) {
-          current = alpha[num];
-        }
-      }
     }
-    console.log("----------------");
-    console.log(current, cnt);
     answer += current;
   });
 
@@ -62,39 +58,7 @@ function solution(s, skip, index) {
   return answer;
 }
 
-const s = "klmnopqrstuvwxyz"; //lmnopqrstuvwxyz
-const skip = "abcdefghij";
-const index = 20;
+const s = "aukks";
+const skip = "wbqd";
+const index = 5;
 solution(s, skip, index);
-
-// split.forEach((x) => {
-//   let current = x.charCodeAt();
-//   let after;
-//   let cnt = 0;
-//   let i = 1;
-//   while (cnt < index) {
-//     after = String.fromCharCode(current + i);
-//     console.log(after, cnt);
-//     if (current + i + 1 > 122) {
-//       // console.log(current, i, index, after, cnt);
-//       current = 97;
-//       i = 0;
-//       if (index - cnt <= 1) {
-//         after = String.fromCharCode(current + i);
-//         break;
-//       }
-//     }
-//     if (skip.indexOf(after) == -1) {
-//       if (after == "{") {
-//         // console.log("a");
-//       }
-//       // console.log(after, cnt);
-//       cnt++;
-//       i++;
-//     } else {
-//       i++;
-//     }
-//   }
-//   console.log("=====================================================");
-//   answer += after;
-// });
