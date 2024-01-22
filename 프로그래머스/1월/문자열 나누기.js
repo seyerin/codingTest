0234964dc196016890682d71dd98ca38e31322f4
seoyerin <seyerin12@naver.com>
2024-01-23 03:13:29 +0900

    코테24

---
+++ 프로그래머스/1월/문자열 나누기.js
@@ -0,0 +1,37 @@
+function solution(s) {
+  var answer = 0;
+  let split = s.split("");
+  let anotherCharCnt = 0;
+  let standardCharCnt = 1;
+  let i = 1;
+  let cnt = 0;
+  let temp = [];
+  let length = split.length;
+
+  while (cnt < length) {
+    let standard = split[0];
+    let next = split[i];
+
+    if (next == standard) {
+      standardCharCnt++;
+    } else {
+      anotherCharCnt++;
+    }
+    i++;
+    cnt++;
+    if (standardCharCnt == anotherCharCnt) {
+      answer++;
+      split.splice(0, standardCharCnt * 2);
+      standardCharCnt = 1;
+      anotherCharCnt = 0;
+      i = 1;
+    }
+  }
+  if (answer == 0) answer++;
+  console.log(answer);
+  return answer;
+}
+
+const s = "aa";
+
+solution(s);
