0d6074a896c63742fe18b8d7df9dfd2c325ef415
seoyerin <seyerin12@naver.com>
2024-01-25 01:32:04 +0900

    코테25

---
+++ 프로그래머스/1월/로또의 최고 순위와 최저 순위.js
@@ -0,0 +1,13 @@
+function solution(lottos, win_nums) {
+  var answer = [];
+  let zeroCnt = lottos.filter((x) => x == 0).length;
+  let matchingNum = lottos.filter((x) => win_nums.indexOf(x) != -1).length;
+  let lank = [6, 6, 5, 4, 3, 2, 1];
+  answer = [lank[matchingNum + zeroCnt], lank[matchingNum]];
+  return answer;
+}
+
+const lottos = [0, 0, 0, 0, 0, 0];
+const win_nums = [31, 10, 45, 1, 6, 19];
+
+solution(lottos, win_nums);
