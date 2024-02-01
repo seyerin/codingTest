100dd4eefb09d5d05840207d4edb823b4233718f
seoyerin <seyerin12@naver.com>
2024-02-02 00:09:00 +0900

    코테 33

--- 프로그래머스/2월/푸드파이터 대회.js
+++ 프로그래머스/2월/푸드파이터 대회.js
@@ -1,50 +1,50 @@
-function solution(food) {
-  var answer = "";
-  let length = 0;
-
-  food.forEach((x, n) => {
-    let num = x;
-    if (x % 2 != 0 && n > 0) {
-      num = x - 1;
-      food[n] = x - 1;
-    }
-    length += num;
-  });
-
-  let cnt = 0;
-  let idx = 1;
-  let temp = [];
-
-  while (Math.round(length / 2) > cnt) {
-    temp[cnt] = idx;
-    temp[length - cnt - 1] = idx;
-
-    if (food[idx] == 0) {
-      idx++;
-      continue;
-    }
-    if (number(temp, idx) % food[idx] == 0) {
-      idx++;
-    }
-    if (cnt == Math.floor(length / 2)) {
-      temp[cnt] = 0;
-      break;
-    }
-    cnt++;
-  }
-  answer = temp.join("");
-  console.log(answer);
-  return answer;
-}
-
 // function solution(food) {
-//   let res = "";
-//   for (let i = 1; i < food.length; i++) {
-//     res += String(i).repeat(Math.floor(food[i] / 2));
+//   var answer = "";
+//   let length = 0;
+
+//   food.forEach((x, n) => {
+//     let num = x;
+//     if (x % 2 != 0 && n > 0) {
+//       num = x - 1;
+//       food[n] = x - 1;
+//     }
+//     length += num;
+//   });
+
+//   let cnt = 0;
+//   let idx = 1;
+//   let temp = [];
+
+//   while (Math.round(length / 2) > cnt) {
+//     temp[cnt] = idx;
+//     temp[length - cnt - 1] = idx;
+
+//     if (food[idx] == 0) {
+//       idx++;
+//       continue;
+//     }
+//     if (number(temp, idx) % food[idx] == 0) {
+//       idx++;
+//     }
+//     if (cnt == Math.floor(length / 2)) {
+//       temp[cnt] = 0;
+//       break;
+//     }
+//     cnt++;
 //   }
-//   console.log(res + "0" + [...res].reverse().join(""));
-//   return res + "0" + [...res].reverse().join("");
+//   answer = temp.join("");
+//   console.log(answer);
+//   return answer;
 // }
+
+function solution(food) {
+  let res = "";
+  for (let i = 1; i < food.length; i++) {
+    res += String(i).repeat(Math.floor(food[i] / 2));
+  }
+  console.log(res + "0" + [...res].reverse().join(""));
+  return res + "0" + [...res].reverse().join("");
+}
 
 /*
   reverse() 쓰면 문자열, 배열 거꾸로 정렬해줌 
