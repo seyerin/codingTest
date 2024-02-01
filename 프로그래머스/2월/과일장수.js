dc111471f2a4129dc6a10db565f47b9cb6986f37
seoyerin <seyerin12@naver.com>
2024-02-01 21:57:15 +0900

    코테 32

---
+++ 프로그래머스/2월/과일장수.js
@@ -0,0 +1,35 @@
+function solution(k, m, score) {
+  var answer = 0;
+  let box = [];
+  let temp = [];
+
+  score.sort((a, b) => b - a);
+
+  score.forEach((x, n) => {
+    temp.push(x);
+    if ((n + 1) % m == 0) {
+      box.push(temp);
+      temp = [];
+    }
+  });
+  console.log(box);
+  if (box.length == 0) return 0;
+
+  box.forEach((x) => {
+    let min = Infinity;
+    x.forEach((y) => {
+      if (min > y) {
+        min = y;
+      }
+    });
+    answer = answer + min * m;
+    min = Infinity;
+  });
+  return answer;
+}
+
+const k = 3;
+const m = 4;
+const score = [1, 2, 3, 1, 2, 3, 1];
+// 5555 4542
+solution(k, m, score);
