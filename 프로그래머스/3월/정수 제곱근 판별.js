d8ceb6cd0d5f34a8e4ba8c4b531c311f2e58f01c
seoyerin <seyerin12@naver.com>
2024-03-07 16:32:44 +0900

    코테 67

---
+++ 프로그래머스/3월/정수 제곱근 판별.js
@@ -0,0 +1,14 @@
+function solution(n) {
+  var answer = 0;
+  let sqrt = Math.sqrt(n);
+  if (Number.isInteger(sqrt)) {
+    answer = Math.pow(sqrt + 1, 2);
+  } else {
+    return -1;
+  }
+  return answer;
+}
+
+let n = 121;
+
+solution(n);
