f12771504d0f852ad71fc2e9ea311c3eb6b1d130
seoyerin <seyerin12@naver.com>
2024-03-09 22:24:31 +0900

    코테 68

---
+++ 프로그래머스/3월/자연수 뒤집어 배열 만들기.js
@@ -0,0 +1,12 @@
+function solution(n) {
+  var answer = [];
+  while (Math.floor(n) != 0) {
+    answer.push(n % 10);
+    n = Math.floor(n / 10);
+  }
+  console.log(answer);
+  return answer;
+}
+
+const n = 2456873;
+solution(n);
