33b86f5ae593583487ae9be67407ade96543d8f7
seoyerin <seyerin12@naver.com>
2024-03-11 18:51:22 +0900

    코테 69

---
+++ 프로그래머스/3월/두 정수의 합.js
@@ -0,0 +1,13 @@
+function solution(a, b) {
+  var answer = 0;
+  for (let i = Math.min(a, b); i <= Math.max(a, b); i++) {
+    answer += i;
+  }
+  console.log(answer);
+  return answer;
+}
+
+const a = 3;
+const b = 5;
+
+solution(a, b);
