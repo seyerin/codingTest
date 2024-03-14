f8aa4c47b840412086bb3f80560299ddc634f507
seoyerin <seyerin12@naver.com>
2024-03-15 00:31:07 +0900

    코테 79

---
+++ 타언어/배열 길이에 따라 다른 연산하기.java
@@ -0,0 +1,21 @@
+class Solution {
+  public int[] solution(int[] arr, int n) {
+      int[] answer = {};
+      for(int i =0; i<arr.length; i++){
+          if(arr.length %2 == 0){
+          if(i %2 != 0){
+              arr[i] += n;
+          }
+      }
+       else{
+           if(i%2 == 0){
+              arr[i] += n; 
+           }
+       }
+      }
+      for(int i = 0; i<arr.length; i++){
+      System.out.println(arr[i]);
+      }
+      return arr;
+  }
+}
