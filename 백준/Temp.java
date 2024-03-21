a4ba13764c7f33bd733026eae1cba77878044c31
seoyerin <seyerin12@naver.com>
2024-03-21 16:07:20 +0900

    코테 82

---
+++ 백준/Temp.java
@@ -0,0 +1,14 @@
+// 1271번 문제
+import java.math.BigInteger;
+import java.util.*;
+
+public class Temp {
+  public static void main(String [] args){
+    Scanner sc = new Scanner(System.in);
+    BigInteger m = sc.nextBigInteger();
+    BigInteger n = sc.nextBigInteger();
+    sc.close();
+    System.out.println(m.divide(n));
+    System.err.println(m.remainder(n));
+  }
+}
