d11c7553f30893115832c9e2d70a08945503f8f4
seoyerin <seyerin12@naver.com>
2024-03-27 00:22:16 +0900

    코테 88

---
+++ 타언어/정수 부분.java
@@ -0,0 +1,11 @@
+public class 정수 부분 {
+  import java.math.*;
+class Solution {
+    public int solution(double flo) {
+        int answer = 0;
+        answer = (int)Math.floor(flo);
+        return answer;
+    }
+}
+  
+}
