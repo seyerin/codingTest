d8f0d3d534a1ec47081b21fcc3c86005f6dc3e25
seoyerin <seyerin12@naver.com>
2024-04-01 00:04:15 +0900

    코테 99 1076

--- 백준/Main.java
+++ 백준/Main.java
@@ -1,24 +1,28 @@
 import java.math.BigInteger;
-import java.util.Date;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    String n = sc.nextLine();
-    int f = sc.nextInt();
+    String color1 = sc.nextLine();
+    String color2 = sc.nextLine();
+    String color3 = sc.nextLine();
 
-    String lastnum = n.charAt(n.length()-2) +""+ n.charAt(n.length()-1);
-    int num = Integer.parseInt(n) - Integer.parseInt(lastnum);
-    
-    for(int i=1; i<=100; i++){
-      if(num%f == 0){
-        break;
-      }
-      num++;
-    }
-    n = Integer.toString(num);
-    lastnum = n.charAt(n.length()-2)+""+n.charAt(n.length()-1);
-    System.out.println(lastnum);
+String[] colorArr = new String[] {
+    "black", "brown", "red","orange","yellow","green", "blue","violet","grey","white"
+};
+
+  int idx1 = Arrays.asList(colorArr).indexOf(color1);
+  int idx2 = Arrays.asList(colorArr).indexOf(color2);
+  int idx3 = Arrays.asList(colorArr).indexOf(color3);
+
+  String result = Integer.toString((idx1*10)+idx2);
+  
+  for(int i = 0; i<idx3; i++){
+    result += "0";
+  }
+  BigInteger n = new BigInteger(result);
+  System.out.println(n);
   }
 }
