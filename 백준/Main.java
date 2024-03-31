135bcc35055526d55284d897b35ee5843dbf5dae
seoyerin <seyerin12@naver.com>
2024-03-31 22:22:59 +0900

    코테 97 10807

--- 백준/Main.java
+++ 백준/Main.java
@@ -5,8 +5,21 @@ import java.util.Scanner;
 public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    BigInteger a = sc.nextBigInteger();
-    BigInteger b = sc.nextBigInteger();
-    System.out.println(a.add(b));
+    int n = sc.nextInt();
+    int[] arr = new int[n];
+
+    for(int i=0; i<n; i++){
+      arr[i] = sc.nextInt();
+    }
+    int v = sc.nextInt();
+
+    int cnt =0; 
+    for (int i : arr) {
+      if(i == v){
+        cnt++;
+      }
+    }
+
+    System.out.println(cnt);
   }
 }
