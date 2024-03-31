6091728a2945c002399a0d6519e708ed86c446d0
seoyerin <seyerin12@naver.com>
2024-03-31 20:34:00 +0900

    코테 93 8393

--- 백준/Main.java
+++ 백준/Main.java
@@ -4,10 +4,12 @@ public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int n = sc.nextInt();
-    int sum =0;
-    for(int i=1; i<=n; i++){
-      sum += i;
+
+    for(int i=0; i<n; i++){
+      String str = sc.nextLine();
+      System.out.println(str);
+        // System.out.println(str.charAt(0) + "" + str.charAt(str.length() -1));
     }
-    System.out.println(sum);
+    
   }
 }
