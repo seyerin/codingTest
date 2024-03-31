c45f356eeb68578b5a06762e9458d7d078df8cb3
seoyerin <seyerin12@naver.com>
2024-03-31 20:27:08 +0900

    코테 92 7891

--- 백준/Main.java
+++ 백준/Main.java
@@ -3,31 +3,11 @@ import java.util.Scanner;
 public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-
-    int a =sc.nextInt();
-    int b =sc.nextInt();
-    int c =sc.nextInt();
-
-    sc.close();
-    int min = 1000000;
-    int max = -1111111;
-
-    min = Math.min(a, b);
-    min = Math.min(min, c);
-
-    max = Math.max(a, b);
-    max = Math.max(max, c);
-
-
-
-    if(min < a & max > a){
-      System.out.println(a);
-    }
-    else if(min < b & max > b){
-      System.out.println(b);
-    }
-    else {
-      System.out.println(c);
+    int n = sc.nextInt();
+    for(int i = 0; i<n; i++){
+      int a = sc.nextInt();
+      int b = sc.nextInt();
+      System.out.println(a+b);
     }
   }
 }
