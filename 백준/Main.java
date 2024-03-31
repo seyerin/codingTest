605ec221a152fc4a6fc9d98b5064b48e52855845
seoyerin <seyerin12@naver.com>
2024-03-31 22:44:28 +0900

    코테 97 1009

--- 백준/Main.java
+++ 백준/Main.java
@@ -6,20 +6,19 @@ public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int n = sc.nextInt();
-    int[] arr = new int[n];
-
     for(int i=0; i<n; i++){
-      arr[i] = sc.nextInt();
-    }
-    int v = sc.nextInt();
-
-    int cnt =0; 
-    for (int i : arr) {
-      if(i == v){
-        cnt++;
+      int a = sc.nextInt();
+      int b = sc.nextInt();
+      int result = 1;
+      for(int j =0; j<b; j++){
+        result = (result*a) %10;
+      }
+      if(result == 0){
+        System.out.println(10);
+      }
+      else{
+        System.out.println(result);
       }
     }
-
-    System.out.println(cnt);
   }
 }
