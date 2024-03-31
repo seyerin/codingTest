8178a7b252475a4c5c1b89142a7abd5c08909ac7
seoyerin <seyerin12@naver.com>
2024-04-01 00:35:18 +0900

    코테 100 1100

--- 백준/Main.java
+++ 백준/Main.java
@@ -12,11 +12,22 @@ public class Main {
     }
     int cnt =0;
 
-    for(int i=0; i<8; i++){
-      for(int j=0; j<8; j++){
-        if(j%2 == 0){
-          if(arr[i].charAt(j) == 'F'){
-            cnt++;
+    for(int i=1; i<=8; i++){
+      if(i%2 == 0){
+        for(int j=1; j<=8; j++){
+          if(j%2 == 0){
+            if(arr[i-1].charAt(j-1) == 'F'){
+              cnt++;
+            }
+          }
+        }
+      }
+      else{
+        for(int j=1; j<=8; j++){
+          if(j%2 != 0){
+            if(arr[i-1].charAt(j-1) == 'F'){
+              cnt++;
+            }
           }
         }
       }
