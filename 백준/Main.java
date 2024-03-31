81b522afb19095afabfc0229fb7652577112c52a
seoyerin <seyerin12@naver.com>
2024-03-31 20:28:41 +0900

    코테 93 8393

--- 백준/Main.java
+++ 백준/Main.java
@@ -4,10 +4,10 @@ public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int n = sc.nextInt();
-    for(int i = 0; i<n; i++){
-      int a = sc.nextInt();
-      int b = sc.nextInt();
-      System.out.println(a+b);
+    int sum =0;
+    for(int i=1; i<=n; i++){
+      sum += i;
     }
+    System.out.println(sum);
   }
 }
