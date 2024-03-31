4ee0443bdd4ff795e5754997a2c14818f078b3b0
seoyerin <seyerin12@naver.com>
2024-03-31 22:18:17 +0900

    코테 95 10757

--- 백준/Main.java
+++ 백준/Main.java
@@ -1,10 +1,13 @@
+import java.math.BigInteger;
 import java.util.Date;
 import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
-    Date date = new Date();
-    // String month = date.getMonth()+1 <10?"0"+date.getMonth()+1:date.getMonth()+1.
-    System.out.println((date.getYear()+1900) +"-"+"0"+(date.getMonth()+1)+"-"+date.getDate());
+    Scanner sc = new Scanner(System.in);
+    BigInteger a = sc.nextBigInteger();
+    BigInteger b = sc.nextBigInteger();
+
+    System.out.println(a.add(b));
   }
 }
