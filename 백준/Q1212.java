9d080f72a6cc74751f7fcf0c6e47d6442d519d52
seoyerin <seyerin12@naver.com>
2024-04-02 02:14:02 +0900

    코테 104

---
+++ 백준/Q1212.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+
+    String n = s.next();
+
+    StringBuilder result = new StringBuilder();
+
+    for(int i=0; i<n.length();i++){
+      String temp = Integer.toBinaryString(n.charAt(i)-'0');
+
+      if(temp.length() == 2 && i!=0){
+        temp = "0"+temp;
+      }
+      else if(temp.length() ==1 && i!=0){
+        temp = "00"+temp;
+      }
+      result.append(temp);
+    }
+    System.out.println(result);
+  }
+}
