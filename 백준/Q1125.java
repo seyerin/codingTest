9fa2059e1357b24911ecb66e892b9e0b9e2ec2c5
seoyerin <seyerin12@naver.com>
2024-04-03 09:37:05 +0900

    코테 105

---
+++ 백준/Q1125.java
@@ -0,0 +1,20 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+
+    String num1 = s.next();
+    String num2 = s.next();
+
+    long result = 0;
+
+    for(int i=0; i<num1.length(); i++){
+      for(int j=0; j<num2.length(); j++){
+        result += (num1.charAt(i)-'0')*(num2.charAt(j)-'0');
+      }
+    }
+    System.out.println(result);
+  }
+}
