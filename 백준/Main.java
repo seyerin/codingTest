4720476ebc77e9b428bea59bc18a55a14d457269
seoyerin <seyerin12@naver.com>
2024-04-07 19:41:11 +0900

    코테 110

--- 백준/Main.java
+++ 백준/Main.java
@@ -1,13 +1,16 @@
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
     Scanner s = new Scanner(System.in);
+    String num = s.next();
 
-    long a = s.nextLong();
-    long b = s.nextLong();
-    long c = s.nextLong();
+    long remainder = 0;
+    for (int i = 0; i < num.length(); i++) {
+        remainder = (remainder * 10 + (num.charAt(i) - '0')) % 20000303;
+    }
 
-    System.out.println(a+b+c);
+    System.out.println(remainder);
   }
 }
